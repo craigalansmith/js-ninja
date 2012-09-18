4aad9738b7835081f4496b301dd3a6da1d199765
Craig Smith <craigsmith@fastmail.fm>
2012-09-18 23:25:09 +0100

    first commit

---
+++ ch1.js
@@ -0,0 +1,36 @@
+// Secrets of the Javascript Ninja
+// John Resig
+
+/*
+Writing effective, clean code.
+An additional problem - the difficulties of making things work cross-browser.
+Standard libraries - jQuery etc - we will study these to gain insight into 1 and 2 above.
+
+The makeup of a js library:
+
+	- advanced use of js language
+	- cross-browser code
+	- best practices
+
+The js language:
+	- objects, functions
+	- anonymous inline functions
+	- closures
+
+A close relationship between objects, functions and closures which must be understood for a good foundation.
+
+Underused language features:
+	- timers
+	- regexes
+	- with statement
+	- eval
+
+Best practices:
+	- testing
+		- assert function: assert(condition, message);
+	- performance analysis
+		- perf function
+	- debugging skills
+
+These are especially important in the restricted js development environment.
+*/
---
+++ ch2.js
@@ -0,0 +1,43 @@
+// ch2 - testing and debugging
+
+/*
+Particularly important where external factors can affect our code ie browser differences.
+
+*/
+
+// 2 approaches to debugging js: logging and breakpoints
+
+// can log to the console using console.log() - much better than alerts
+
+// cross-browser console.log
+function log() {
+    try {
+        // the most common method
+
+        // note here we use the apply method of the js Function
+        // to relay the arguments passed to this function
+        // to console.log
+		console.log.apply(console, arguments);
+	}
+	catch(e) {
+		try {
+            // try the opera way
+			opera.postError.apply(opera, arguments);
+		}
+		catch(e) {
+            // if all else fails then alert
+
+            // here we use the call method of the js Function
+			alert(Array.prototype.join.call(arguments, " "));
+		}
+	}
+}
+
+// Test generation
+
+// Good tests are:
+    - repeatable
+    - simple
+    - independent
+
+// deconstructive vs constructive
