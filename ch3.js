2e105fd0ef6bdb54835238f69abab7d939ee0df4
Craig Smith <craigsmith@fastmail.fm>
2012-09-23 22:15:56 +0100

    function context, call() and apply()

--- ch3.js
+++ ch3.js
@@ -127,6 +127,74 @@ katana();
 
 assert(isSharp === true, "a global property now exists"); // important to be aware of what a function's context is
 
+// one more time - the context of a function is determined by how it is invoked
+
+// call() and apply() allow us to define the context (as well as the arguments) when we call functions:
+
+var getContext = function() { 
+	return this; 
+}
+
+someObject = {};
+
+assert(getContext() == window, "the context is the global object");
+assert(getContext.call(someObject) == someObject, "call() was used to change the context to a specific object");
+
+// call and apply similar, but call accepts arguments individually, while apply takes an array of arguments
+
+function add(a, b) {
+	return a + b;
+}
+
+assert(add.call(this, 1, 2) == 3, "call() takes args individually");
+assert(add.apply(this, [1, 2]) == 3, "apply() takes an array of args");
+
+// a use for call() / apply() - callbacks
+
+// call the given function passing in each array element. stop if the function returns false.
+function each(array, fn) {
+	for (var i = 0; i < array.length; i++)
+		if (fn.call(array, array[i], i) === false)
+			break;
+}
+
+var num = 0;
+var numbers = [4, 5, 6];
+
+each(numbers, function(value, n) {
+	assert(this === numbers, "Context is correct");
+	assert(n == num++, "Counter as expected");
+	assert(value = numbers[n], "Value as expected");
+});
+
+// another example of the uses of configuring the function context using call()
+//
+// a collection of DOM elements, reusing functionality from the javascript Array object
+
+var elements = {
+	length: 0,
+	add: function(element) {
+		Array.prototype.push.call(this, element);
+	},
+	find: function(id) {
+		this.add(document.getElementById(id));
+	}
+};
+
+elements.find("results");
+assert(elements.length == 1 && elements[0].nodeType, "verify that we have an element in our collection");
+
+elements.find("results");
+assert(elements.length == 2 && elements[1].nodeType, "verify the second insertion");
+
+// (on page 51)
+
+
+
+
+
+
+
 
 
 		
