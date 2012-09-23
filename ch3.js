c7adebe3cf9b264aac977981040bbbaa82d56a98
Craig Smith <craigsmith@fastmail.fm>
2012-09-23 18:25:48 +0100

    git ignore and a few more ch 3 notes

--- ch3.js
+++ ch3.js
@@ -1,27 +1,69 @@
 // chapter 3 - Functions are fundamental
+
+$(document).ready(function() {
 //
 // most of this done elsewhere so to recap:
 //
 // 1. functions are first-class objects - they can be treated as any other object - passed, assigned declared literally etc.
 //
+// (3.2 - Anonymous functions)
+//
 // 2. anonymous functions are used extensively in javascript. (make code more compact, easier to read, put function declaration and use closer together, avoid introducing redundant names.)
 //
 // 3. there are three ways to declare them:
 
-function isAnimal() { return true; }			// named function
+function isAnimal() { 
+	// where are we anyway?
+	log(this);
+	return true; }			// named function
 
-var isVegetable = function() { return true; } 	// declared as a variable
+var isVegetable = function() { 
+	// where are we anyway?
+	log(this);
+	return true; } 	// declared as a variable
 
-window.isMineral = function() { return true; }	// declared as a property
+window.isMineral = function() { 
+	// where are we anyway?
+	log(this);
+	return true; }	// declared as a property
 
-log(window.isAnimal());
-log(window.isVegetable());
-log(window.isMineral());
+log(isAnimal());
+log(isVegetable());
+log(isMineral());
+log(this);
 
 // 4. only named functions can be forward-referenced.
 //
 // 5. functions are objects. anonymous functions don't have names (for named functions, the name is stored in the name property of the function object.)
 //
-// 6. functions have context - "this". so for example, if we had an anonymous, recursive function, the recursive call would be made via "this".
+// 6. functions have context - "this".
 //
-// 
+// 7. when we use this in a method, it points to the object through which the method was invoked.
+//
+// 8. confusingly, anonymous functions can be given a name. with these "inline functions" however, the name is only visible within the function itself.
+// 		- so this is a second way of referencing "this" function.
+//
+// 9. and a third way - arguments.callee. the arguments ref is available within every function and points to the arg list. the callee property is a ref to the function.
+//
+// (3.3 Functions as Objects)
+//
+// 10. functions are objects - eg. they have properties and we can add new properties to them. one application of this would be in callback management. if functions can be
+// 			added to a list of functions to be called back, then we don't want to allow duplicates. This could be achieved by assigning to a newly created property on the
+// 			function when it is added, then check the value of this before adding to the collection:
+
+var store = {
+	nextId: 1,
+	cache: {},
+	add: function(fn) {
+		if (!fn.id) {
+			fn.id = store.nextId++;
+			return !!(store.cache[fn.id] = fn);	
+			// the !! operator converts any expression to it's boolean equivalent - in this case, the value of a function which will always be true
+		}
+	}
+};
+
+
+
+// end document ready
+});
