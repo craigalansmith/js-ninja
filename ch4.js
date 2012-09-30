e47f57a78fb68a779edf6de2224d7a85b92bc423
Craig Smith <craigsmith@fastmail.fm>
2012-09-30 23:26:59 +0100

    closures for private variables

--- ch4.js
+++ ch4.js
@@ -37,7 +37,56 @@ $(document).ready(function() {
 
 	// 4.1.1 - first use for closures - creating private variables
 	
-	// (page 65)
+	// first define a function which will serve as a wrapper round the variables defined within - an object
+	function Cake() {
+		var slices = 0;
+
+		this.getSlices = function() {
+			return slices;
+		};
+
+		this.slice = function() {
+			slices++;
+		};
+	}
+	
+	// use the new keyword with the wrapper function / object
+	var cake = new Cake();
+	cake.slice();
+
+	assert(cake.getSlices() == 1, "we are able to access the internal slice data.");
+
+	assert(cake.slices == undefined, "and the private data is inaccessible to us.");
+	
+	// 4.1.2 - callbacks and timers
+	
+	var elem = $("div");
+	elem.html("loading...");
+
+	$.ajax({
+		url: "main.html",
+		success: function(html) {
+			assert(elem, "the element to append to, via a closure.");
+			elem.html(html); // save doing another DOM query by using the closure to access the element.
+		}
+	});
+	
+	// using a closure from a timer interval
+	var elem = document.getElementById("box");
+	var count = 0;
+
+	var timer = setInterval(function() {
+		if (count < 100) {
+			elem.style.left = count + "px";
+			count++;
+		} else {
+			assert(count == 100, "count came via a closure, accessed each step.");
+			
+			assert(timer, "the timer reference is also via a closure");
+
+			clearInterval(timer);
+		}
+	}, 10);	
 
 });
 
